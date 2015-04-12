77d27f1f8ec2ee2b33e4a90a98590401b18f5f78
shivangpatel <patelshivang82@gmail.com>
2015-04-12 12:34:55 +0530

    added new files...

---
+++ python_12April2015/functions.py
@@ -0,0 +1,14 @@
+def hours():
+
+ h = int(raw_input('Enter the number of hours : '))
+ return h
+
+def days():
+
+ d = int(raw_input('Enter the number of days : '))
+ return d
+
+def cost(h,d):
+
+ cost = h*d*8
+ return cost
---
+++ python_12April2015/main.py
@@ -0,0 +1,6 @@
+import functions
+
+h = functions.hours()
+d = functions.days()
+cost = functions.cost(h,d)
+print "Total Cost is : " + str(cost)
